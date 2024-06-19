d121486bb523ccc938154ea55781eb4136e0bf6b
mitsuko yoneyama <latteforever@gmail.com>
2024-06-18 17:07:19 -0700

    first commit

---
+++ components/Test.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div><h1 className="text-20 font-bold text-white-1">
+    RightSide Bar</h1></div>
+  )
+}
+
+export default Test
---
+++ next.config.mjs
@@ -0,0 +1,31 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    typescript: {
+      ignoreBuildErrors: true
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'lovely-flamingo-139.convex.cloud'
+        },
+        {
+          protocol: 'https',
+          hostname: 'sleek-capybara-771.convex.cloud'
+        },
+        {
+          protocol: 'https',
+          hostname: 'blissful-ram-40.convex.cloud'
+        },
+        {
+          protocol: 'https',
+          hostname: 'img.clerk.com'
+        },
+      ]
+    }
+  };
+  
+  export default nextConfig;
+  
+
+
